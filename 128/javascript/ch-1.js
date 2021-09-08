568954072102c23d6005947f03794553f6b0bc1c
Ian Goodnight <goodnight.ian@gmail.com>
2021-09-08 11:31:00 -0400

    missed the deadline

--- 128/javascript/ch-1.js
+++ 128/javascript/ch-1.js
@@ -60,14 +60,141 @@ const output = [
 ];
 
 function findSubMatrix(matrix = [[]]) {
-  const matrixes = [];
+  let optimal = [0, 0];
+  let tests = [];
+  const area = ([x, y]) => x * y;
+  let contenders = [];
 
-  matrix.forEach((row) => {
+  for (let i = 0; i < matrix.length; i++) {
+    let test = matrix.slice(i);
 
-  })
+    for (let j = 0; j < matrix.length; j++) {
+      let testSlice = test.map(x => x.slice(j));
+      // console.log('test slice: ');
+      /*(() => {
+        testSlice.forEach(row => console.log(row, "\n"));
+        // console.log('=======');
+      })();*/
+      let champion = reduceToArea(reduceToX(testSlice));
+      // console.log('champion: ', champion, '\n\n');
+      contenders.push(champion);
+    }
+  }
+
+  const kingOfTheHill = contenders.reduce((king, kid) => {
+    // console.log(king, ' vs ', kid);
+    if (area(king) > area(kid)) {
+      // console.log('Winner: ', king);
+      return king;
+    }
+    if (area(king) === area(kid)) {
+      if (king[0] < kid[0]) {
+        // console.log('Close one! ', kid);
+        return kid;
+      }
+      // console.log('Close one!', king);
+    }
+    // console.log('Winner: ', kid);
+    return kid;
+  }, [0, 0]);
+
+  // console.log(kingOfTheHill);
+  return fillZeroes(kingOfTheHill);
 }
 
-findSubMatrix(input);
+function fillZeroes([length, count]) {
+  const container = [];
+  while (count > 0) {
+    const row = Array(length).fill(0);
+    container.push(row);
+    count--;
+  }
+  return container;
+}
+
+const test = [
+  [ 0, 0, 1 ],
+  [ 0, 0, 0 ],
+  [ 0, 1, 1 ],
+];
+
+function reduceToLeft (list = [], match = 0) {
+  const test = [...list];
+
+  let matched = 0;
+  while (test.length > 0) {
+    if (test.shift() !== match) return matched;
+    matched++;
+  }
+  return matched;
+}
+
+function reduceToX (list = [[]], match = 0) {
+  return list.reduce((reduced, record) => {
+    reduced.push(reduceToLeft(record, match));
+    return reduced;
+  }, []);
+}
+
+function reduceToArea (reducedX = []) {
+  const xVals = [...reducedX];
+
+  // console.log('xVals: ', xVals);
+
+  let highWaterMark = xVals.shift();
+  if (highWaterMark < 1) return [0, 0];
+  let y = 1;
+
+  let optimal = [highWaterMark, y];
+  y++
+
+  while (xVals.length > 0 && xVals[0] > 0) {
+    // console.log('highWaterMark: ', highWaterMark);
+    let x = xVals.shift();
+    // console.log('x y: ', x, y);
+
+    if (x >= highWaterMark) {
+      optimal = [highWaterMark, y];
+    }
+    if ((x * y) > (optimal[0] * optimal[1])) {
+      optimal = [highWaterMark, y];
+    }
+    y++;
+  }
+  return optimal;
+}
+
+function creep(swamp = [[]]) {
+  const area = [0, 0];
+
+  let xBound = 0;
+  let yBound = 0;
+
+  if (swamp[xBound][yBound] === 0) {
+    let [x ,y] = area;
+    x++;
+    y++;
+    xBound++;
+    yBound++;
+
+  }
+}
+
+function find0Range(row = []) {
+  let range = 0;
+  while (row[range] === 0) {
+    range++;
+  }
+  return range;
+}
+
+function is2dArray (input) {
+  if (!Array.isArray(input)) return false;
+  return input.reduce((pass, test) => {
+    if (pass === false || !Array.isArray(test)) pass = false;
+    return pass;
+  }, true);
+}
 
 /**
  * Followed by some utilities to test our solution
@@ -122,6 +249,10 @@ function assertMatchingMatrixes(input = [[]], output = [[]]) {
   output.forEach((row) => {
     console.log(row);
   });
+  console.log('Received:');
+  test.forEach((row) => {
+    console.log(row);
+  });
 
   if (compareMatrixes(output, test)) {
     return console.log('\x1b[32m%s\x1b[0m', 'Passed \u2690');
@@ -148,7 +279,7 @@ const isDirectory = (filePath) => fs.lstatSync(filePath).isDirectory();
 
       return assertMatchingMatrixes(input, output);
     }
-    if (isDirectory()) {
+    if (isDirectory(testPath)) {
       fs.readdirSync(testPath).map((fileName) => {
         const filePath = path.join(testPath, fileName);
 
@@ -164,4 +295,4 @@ const isDirectory = (filePath) => fs.lstatSync(filePath).isDirectory();
   } catch (error) {
     console.log('Something went wrong: ', error);
   }
-})/*()*/;
+})();
