5592b3bed9b869bd15b510727961a3a5e7b117ea
Ian Goodnight <goodnight.ian@gmail.com>
2021-10-06 21:21:09 -0400

    perl solutions done

--- 133/perl/ch-2.pl
+++ 133/perl/ch-2.pl
@@ -22,6 +22,7 @@
 use strict;
 use warnings;
 use utf8;
+use Data::Dumper;
 
 ################################################################################
 # Our PWC solution, along with some help subroutines
@@ -37,12 +38,106 @@ sub prime_factors {
     |           # Alternating with
     \s+         # Trailing whitespace
     \z          # End of line
-  }{}gx
+  }{}gx;
+
   # Validate our input is an integer
-  if (!$number =~ m/\A\d+\z/) {
+  if ( !$number =~ m/\A\d+\z/ ) {
+
     # Bail
     return 0;
   }
   my @factors;
-  my $divisor = 2  # Starting with 2, we'll divide and check for modulo
+  my $divisor = 2;    # Starting with 2, we'll divide and check for modulo
+
+  while ( $number >= 2 ) {
+    if ( $number % $divisor == 0 ) {
+
+      # If modulo is zero, push $divisor to @factors
+      push @factors, $divisor;
+      $number /= $divisor;
+    }
+    else {
+      # Else, increment $divisor and try again
+      $divisor += 1;
+    }
+  }
+  return \@factors;
 }
+
+# Helper to reduce number to sum of its digits
+sub sum_digits {
+  my $number = shift;
+
+  # Trim
+  $number =~ s{ \A \s+ | \s+ \z }{}gx;
+
+  # Split digits
+  my @digits = split //, $number;
+
+  # Reduce
+  my $sum = 0;
+  foreach my $digit (@digits) {
+    $sum += $digit;
+  }
+  return $sum;
+}
+
+# Find `Smith Numbers` with the help of our two subroutines, `prime_factors`
+# and `sum_digits`
+sub find_smith_numbers {
+
+  # We need to find the first ten, but we might as well write it to find more
+  my $limit = shift // '10';
+  my @smith_numbers;
+
+  # Smith Numbers are not prime numbers, so we might as well start at 4
+  my $test = '4';
+
+  # Search until we hit our limit
+  while ( scalar @smith_numbers < $limit ) {
+    my @primes     = @{ prime_factors($test) };
+    my $factor_sum = 0;
+
+    # Reduce @primes to sum of its digits
+    foreach my $prime (@primes) {
+      $factor_sum += sum_digits($prime);
+    }
+
+    my $digit_sum = sum_digits($test);
+
+    # Check for matching sums and for prime number (if scalar @primes == 1,
+    # $test is a prime number)
+    if ( $factor_sum == $digit_sum && scalar @primes != 1 ) {
+      push @smith_numbers, $test;
+    }
+
+    $test += 1;
+  }
+  return \@smith_numbers;
+}
+
+################################################################################
+# Utilities
+################################################################################
+
+sub list_with_oxford {
+  my @list        = @{ +shift };
+  my $last_number = $list[-1];
+
+  return join( ', ', @list[ 0 .. $#list - 1 ] ) . ', and ' . $last_number;
+}
+
+################################################################################
+# Main
+################################################################################
+
+sub main {
+  my $limit         = shift @ARGV // '10';
+  my $smith_numbers = find_smith_numbers $limit;
+  my $result_string = list_with_oxford $smith_numbers;
+
+  print "The first $limit Smith Numbers are $result_string.\n";
+  return 1;
+}
+
+main();
---
+++ 133/ruby/ch-1.rb
@@ -0,0 +1,41 @@
+#!/usr/bin/env ruby
+# ch-1.rb
+
+=begin
+
+ * https://theweeklychallenge.org/blog/perl-weekly-challenge-133/
+ *
+ * Task 1 > Integer Square Root
+ * ============================
+ *
+ * You are given a positive integer `$N`.
+ *
+ * Write a script to calculate the integer square root of the given number.
+ *
+ * Please avoid using built-in function.  Find out more about it here
+ * (https://en.wikipedia.org/wiki/Integer_square_root).
+ *
+ * Examples
+ * --------
+ *
+ * Input: $N = 10
+ * Output: 3
+ *
+ * Input: $N = 27
+ * Output: 5
+ *
+ * Input: $N = 85
+ * Output: 9
+ *
+ * Input: $N = 101
+ * Output: 10
+
+=end
+
+################################################################################
+# Our PWC Solution
+################################################################################
+
+def int_sqr_root 
+
+end
