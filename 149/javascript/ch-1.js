ddb26428b91a09e0788e3511b7e83cefae66af80
Ian Goodnight <goodnight.ian@gmail.com>
2022-08-17 23:00:01 -0400

    test

---
+++ 149/javascript/ch-1.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node
+// ch-1.js
+
+/*******************************************************************************
+ * https://theweeklychallenge.org/blog/perl-weekly-challenge-149/
+ *
+ * ## Task 1 > Fibonacci Digit Sum
+ * ===============================
+ *
+ * Given an input `$N`, generate the first `$N` numbers for which the sum of
+ * their digits is a Fibonacci number.
+ *
+ * ### Example:
+ * ============
+ *
+ * ```
+ * f(20)=[0, 1, 2, 3, 5, 8, 10, 11, 12, 14, 17, 20, 21, 23, 26, 30, 32, 35, 41, 44]
+ * ```
+ *
+ ******************************************************************************/
+
+'use strict';
+
+class Fibonacci {
+  constructor(nth = 1) {}
+}
